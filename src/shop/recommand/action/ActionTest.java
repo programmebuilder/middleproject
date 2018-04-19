f966b4ea47e270d2e9fbae4b08bbfccd5845db0e
한준희 <hashmel91@gmail.com>
2018-04-19 10:33:15 +0900

    기본세팅(부트스트랩,jquery) 등
    부트

---
+++ src/shop/recommand/action/ActionTest.java
@@ -0,0 +1,10 @@
+package shop.recommand.action;
+
+public class ActionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("action test");
+	}
+
+}
